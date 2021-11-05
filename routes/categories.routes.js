eeea162f63bce6b05990fc3fffd139a64ea26a61
Carlos Guedez <cguedez@efiempresa.com>
2021-11-05 18:42:58 -0400

    config trycatch in categories route

--- routes/categories.routes.js
+++ routes/categories.routes.js
@@ -3,65 +3,125 @@ const express = require('express'),
       CategoryService = require('../services/categories.service'),
       service = new CategoryService;
 
-router.get('/', (req, res) => {
-  const categories = service.find()
-  res
-    .status(200)
-    .json({
-      data: categories
-    })
+router.get('/', async (req, res) => {
+  try {
+    const categories = await service.find()
+    res
+      .status(200)
+      .json({
+        data: categories
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error: {
+          status: 500,
+          title: "internal server error",
+          details: "internal server error"
+        }
+      });
+  }
 });
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-  const category = service.findOne(id)
-  if(!category) {
+router.get('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const category = await service.findOne(id);
+    if(!category) {
+      return res
+        .status(404)
+        .json({
+          error: {
+            status: 404,
+            title: "not found",
+            details: "category not found"
+          }
+        });
+    }
+    return res
+      .status(200)
+      .json({
+        data: category
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error: {
+          status: 500,
+          title: "internal server error",
+          details: "internal server error"
+        }
+      });
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const { body } = req
+    const newCategory = await service.create(body)
+    res
+      .status(201)
+      .json({
+        data: newCategory
+      })
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        error: {
+          status: 500,
+          title: "internal server error",
+          details: "internal server error"
+        }
+      })
+  }
+});
+
+router.patch('/:id', async (req, res) => {
+  try {
+    const body = req.body;
+    const { id } = req.params;
+    const category = await service.update(id, body)
+    res
+      .status(200)
+      .json({
+        data: category
+      })
+  } catch (error) {
     res
       .status(404)
       .json({
         error: {
           status: 404,
           title: "not found",
-          details: "category not found"
+          details: error.message
         }
       })
   }
-  res
-    .status(200)
-    .json({
-      data: category
-    })
 });
 
-router.post('/', (req, res) => {
-  const { body } = req
-  const newCategory = service.create(body)
-  res
-    .status(201)
-    .json({
-      data: newCategory
-    })
-});
-
-router.patch('/:id', (req, res) => {
-  const body = req.body;
-  const { id } = req.params;
-  const category = service.update(id, body)
-  res
-    .status(200)
-    .json({
-      data: category
-    })
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params
-  const category = service.destroy(id)
-  res
-    .status(200)
-    .json({
-      id: category
-    })
+router.delete('/:id', async (req, res) => {
+  try {
+    const { id } = req.params
+    const category = await service.destroy(id)
+    res
+      .status(200)
+      .json({
+        id: category
+      })
+  } catch (error) {
+    res
+      .status(404)
+      .json({
+        error: {
+          status: 404,
+          title: "not fount",
+          details: error.message
+        }
+      })
+  }
 });
 
 
