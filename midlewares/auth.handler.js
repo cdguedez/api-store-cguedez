85cf849ec00151cb444d4ad3c3f4b662bfd90d7e
cdguedez <cdgr.ing@gmail.com>
2022-02-22 19:53:30 -0400

    feat: add middleware for checkApiKey

---
+++ midlewares/auth.handler.js
@@ -0,0 +1,13 @@
+const boom = require('@hapi/boom'),
+      config = require('./../config/config')
+
+class Auth {
+  static checkApiKey(req, res, next) {
+    const apiKey = req.headers['api'];
+    if (!apiKey) { return next(boom.unauthorized('apiKey required')) }
+    if (apiKey !== config.apiKey) { return next(boom.unauthorized('Unauthorized')) }
+    return next();
+  }
+}
+
+module.exports = Auth;
