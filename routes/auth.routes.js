fbc75746bbe6d6d7a212407d315329ff6cab9f6a
cdguedez <cdgr.ing@gmail.com>
2022-05-31 14:00:08 -0400

    [FEATURE] add endpoint by login and register

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,9 +2,14 @@ const express = require('express'),
       router = express.Router(),
       passport = require('passport'),
       jwt = require('jsonwebtoken'),
-      config = require('../config/config')
+      validator = require('./../midlewares/validator.handler'),
+      { login, register } = require('./../schemas/auth.schema'),
+      UsersService = require('./../services/users.service'),
+      config = require('../config/config'),
+      service = new UsersService()
 
 router.post('/login',
+  validator.validatorHandler(login, 'body'),
   passport.authenticate('local', { session: false }),
   async (req, res, next) => {
     try {
@@ -14,6 +19,20 @@ router.post('/login',
       res
         .status(200)
         .json({ data: { user, token }})
+    } catch (error) {
+      next(error)
+    }
+  }
+)
+router.post('/register',
+  validator.validatorHandler(register, 'body'),
+  async(req, res, next) => {
+    try {
+      const  { body } = req
+      const user = await service.create(body)
+      res
+        .status(200)
+        .json({ data: user })
     } catch (error) {
       next(error)
     }
---
+++ schemas/auth.schema.js
@@ -0,0 +1,18 @@
+const Joi = require('joi')
+
+const email = Joi.string().email(),
+userName = Joi.string().min(5).max(20),
+password = Joi.string().min(8)
+
+const login = Joi.object({
+  email: email.required(),
+  password: password.required()
+})
+
+const register = Joi.object({
+  email: email.required(),
+  userName: userName.required(),
+  password: password.required(),
+})
+
+module.exports = { login, register }
