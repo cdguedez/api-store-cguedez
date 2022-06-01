ff7f98b87784b16c8cf055c942679883bf926201
cdguedez <cdgr.ing@gmail.com>
2022-05-31 21:31:32 -0400

    [FEATURE] add endpoint to recory and resetpassword

--- services/auth.service.js
+++ services/auth.service.js
@@ -15,6 +15,7 @@ class AuthService {
       throw boom.unauthorized('unauthorized')
     }
     delete user.dataValues.password
+    delete user.dataValues.recoveryToken
     return user
   }
 
@@ -24,8 +25,41 @@ class AuthService {
     return { user, token }
   }
 
-  async sendMail(email) {
+  async resetPassword(token, newPassword) {
+    try {
+      const payload = jwt.verify(token, config.keyScret)
+      const user = await service.findOne(payload.sub)
+      if(user.recoveryToken !== token) {
+        throw boom.unauthorized('unauthorized')
+      }
+      const isMatch = await bcrypt.compare(newPassword, user.password)
+      if(isMatch) throw boom.notAcceptable('the password has been used before')
+      const hash = await bcrypt.hash(newPassword, 10)
+      await service.update(user.id, { recoveryToken: null, password: hash })
+      return { message: 'password changed' }
+    } catch (error) {
+      throw boom.unauthorized('unauthorized')
+    }
+  }
+
+  async sendRecovery(email) {
     const user = await service.findByEmail(email)
+    const payload = { sub: user.id }
+    const token = jwt.sign(payload, config.keyScret, { expiresIn: '10min' })
+    const link = `http://localhost:3000/recovery?token=${token}`
+    await service.update(user.id, { recoveryToken: token })
+    const info = {
+      from: `"Carlos Guedez 👻" ${config.smtpEmail}`, // sender address
+      to: `${user.email}`, // list of receivers
+      subject: `Hello ${user.userName} this email its for recovery password`, // Subject line
+      text: "get into in this link for password changed", // plain text body
+      html: `<b>get into in this <a href=${link}>Link</a> for password changed  </b>`, // html body
+    }
+    const rta = await this.sendMail(info)
+    return rta
+  }
+
+  async sendMail(infoMail) {
     const transporter = nodemailer.createTransport({
       host: config.smtpHost,
       port: config.smtpPort,
@@ -35,16 +69,9 @@ class AuthService {
         pass: config.smtpPass, // generated ethereal password
       },
     })
-    const info = await transporter.sendMail({
-      from: `"Carlos Guedez 👻" ${config.smtpEmail}`, // sender address
-      to: `${user.email}`, // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    })
-    return info
+    const info = await transporter.sendMail(infoMail)
+    return info.accepted
   }
-
 }
 
 module.exports = AuthService
