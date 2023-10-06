16371d967ff8b0a4e9a520fcf31da50a207bff8e
Carlos Guedez <cdgr.ing@gmail.com>
2023-10-06 14:52:13 -0400

    FIX: change urlDb for environment

--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,10 +1,9 @@
 const { Sequelize } = require('sequelize'),
       config = require('../config/config'),
       setupModels = require('./../db/models'),
-      // USER = encodeURIComponent(config.dbUser),
-      // PASSWORD = encodeURIComponent(config.dbPass),
-      URI = config.dbUrl
-      // URI = `${config.dialect}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
+      USER = encodeURIComponent(config.dbUser),
+      PASSWORD = encodeURIComponent(config.dbPass),
+      URI = `${config.dialect}://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 const sequelize = new Sequelize(URI, {
   dialect: `${config.dialect}`,
@@ -12,5 +11,4 @@ const sequelize = new Sequelize(URI, {
 })
 
 setupModels(sequelize)
-// sequelize.sync()
 module.exports = sequelize
